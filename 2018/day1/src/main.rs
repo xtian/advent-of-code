39ac7ec913d683bd6572cd77fe2f4f6469072848
Christian Wesselhoeft <hi@xtian.us>
2018-12-01 14:20:13 -0700

    Complete Day 1, Puzzle 2

--- 2018/day1/src/main.rs
+++ 2018/day1/src/main.rs
@@ -4,31 +4,71 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 fn main() -> Result<(), &'static str> {
-    let filename = env::args().skip(1).next().ok_or("Input file required")?;
+    let mut args = env::args().skip(1);
+
+    let command = args.next().ok_or("Command required")?;
+    let filename = args.next().ok_or("Input file required")?;
     let file = File::open(filename).map_err(|_| "Could not open input file")?;
 
-    println!("{}", sum_lines(file));
+    let output = match command.as_str() {
+        "sum" => Ok(sum_lines(file)),
+        "repeated" => Ok(find_repeated_value(file)),
+        _ => Err("Unrecognized command"),
+    }?;
+
+    println!("{}", output);
 
     Ok(())
 }
 
 fn sum_lines(input: impl Read) -> isize {
+    get_inputs(input).sum()
+}
+
+fn find_repeated_value(input: impl Read) -> isize {
+    let mut current_value = 0;
+    let mut past_values = vec![0];
+
+    let inputs = get_inputs(input).collect::<Vec<_>>();
+    let mut cycle = inputs.iter().cycle();
+
+    loop {
+        current_value += cycle.next().unwrap();
+
+        if past_values.contains(&current_value) {
+            return current_value;
+        } else {
+            past_values.push(current_value);
+        }
+    }
+}
+
+fn get_inputs(input: impl Read) -> impl Iterator<Item = isize> {
     BufReader::new(input)
         .lines()
-        .filter_map(|line| line.ok())
-        .fold(0, |acc, line| acc + line.parse::<isize>().unwrap_or(0))
+        .filter_map(|line| line.ok().and_then(|l| l.parse::<isize>().ok()))
 }
 
 #[test]
-fn handles_zero() {
+fn finds_repeated_values() {
+    let seq = "+1\n-1".as_bytes();
+    assert_eq!(find_repeated_value(seq), 0);
+
+    let seq = "+3\n+3\n+4\n-2\n-4".as_bytes();
+    assert_eq!(find_repeated_value(seq), 10);
+
+    let seq = "-6\n+3\n+8\n+5\n-6".as_bytes();
+    assert_eq!(find_repeated_value(seq), 5);
+
+    let seq = "+7\n+7\n-2\n-7\n-4".as_bytes();
+    assert_eq!(find_repeated_value(seq), 14);
+}
+
+#[test]
+fn sums_lines() {
     let seq = "0".as_bytes();
-
     assert_eq!(sum_lines(seq), 0);
-}
 
-#[test]
-fn handles_sequence() {
     let seq = "+4\n-2\n-1\n".as_bytes();
-
     assert_eq!(sum_lines(seq), 1);
 }
