287608e8c4200903ada2a3f7a867804a3026fea8
Christian Wesselhoeft <hi@xtian.us>
2018-12-05 13:17:00 -0700

    Complete Day 5, Puzzle 2

--- 2018/rust/day5/src/main.rs
+++ 2018/rust/day5/src/main.rs
@@ -6,6 +6,7 @@ use std::io::BufReader;
 fn main() -> Result<(), &'static str> {
     let mut args = env::args().skip(1);
 
+    let command = args.next().ok_or("Command required")?;
     let filename = args.next().ok_or("Input file required")?;
     let file = File::open(filename).map_err(|_| "Could not open input file")?;
 
@@ -15,9 +16,30 @@ fn main() -> Result<(), &'static str> {
         .read_line(&mut string)
         .map_err(|_| "Could not read from file")?;
 
-    println!("{}", process_polymer(string).trim().len());
+    let output = match command.as_str() {
+        "process" => Ok(process_polymer(string).trim().len()),
+        "process-with-removal" => shortest_with_removal(string).ok_or("Error finding shortest"),
+        _ => Err("Unrecognized command"),
+    }?;
+
+    println!("{}", output);
 
     Ok(())
+}
+
+fn shortest_with_removal(polymer: String) -> Option<usize> {
+    (b'a'..=b'z')
+        .map(|c| {
+            let c = c as char;
+            let c_ = c.to_uppercase().next().unwrap();
+
+            let mut polymer = polymer.clone();
+
+            polymer = polymer.replace(c, "");
+            polymer = polymer.replace(c_, "");
+
+            process_polymer(polymer).trim().len()
+        }).min()
 }
 
 fn process_polymer(mut polymer: String) -> String {
@@ -37,6 +59,14 @@ fn process_polymer(mut polymer: String) -> String {
     }
 
     polymer
+}
+
+#[test]
+fn finds_shortest_polymer_with_removal() {
+    assert_eq!(
+        shortest_with_removal("dabAcCaCBAcCcaDA".to_string()),
+        Some(4)
+    );
 }
 
 #[test]
