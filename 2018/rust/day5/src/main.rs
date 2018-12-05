dd6a7c0af5a12bbc6ee9dfbcf589204cff8bcea3
Christian Wesselhoeft <hi@xtian.us>
2018-12-05 08:35:48 -0700

    Complete Day 5, Puzzle 1

--- 2018/rust/day5/src/main.rs
+++ 2018/rust/day5/src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() -> Result<(), &'static str> {
+    let mut args = env::args().skip(1);
+
+    let filename = args.next().ok_or("Input file required")?;
+    let file = File::open(filename).map_err(|_| "Could not open input file")?;
+
+    let mut string = String::new();
+
+    BufReader::new(file)
+        .read_line(&mut string)
+        .map_err(|_| "Could not read from file")?;
+
+    println!("{}", process_polymer(string).trim().len());
+
+    Ok(())
+}
+
+fn process_polymer(mut polymer: String) -> String {
+    let pairs: Vec<_> = (b'a'..=b'z')
+        .map(|c| c as char)
+        .flat_map(|c| {
+            vec![
+                format!("{}{}", c, c.to_uppercase()),
+                format!("{}{}", c.to_uppercase(), c),
+            ]
+        }).collect();
+
+    while pairs.iter().any(|s| polymer.contains(s)) {
+        for s in pairs.iter() {
+            polymer = polymer.replace(s, "");
+        }
+    }
+
+    polymer
+}
+
+#[test]
+fn processes_polymer() {
+    assert_eq!(process_polymer("aA".to_string()), "");
+    assert_eq!(process_polymer("abBA".to_string()), "");
+    assert_eq!(process_polymer("aabAAB".to_string()), "aabAAB");
+    assert_eq!(
+        process_polymer("dabAcCaCBAcCcaDA".to_string()),
+        "dabCBAcaDA"
+    );
 }
